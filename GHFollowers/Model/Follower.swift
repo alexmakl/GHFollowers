e9996d9ac55f6585313b9b505b734f794c331ff9
Alexander Maklakov <richmak@ya.ru>
2021-08-19 08:43:35 +0300

    Initial Commit

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Alexander on 09.08.2021.
+//
+
+import Foundation
+
+struct Follower: Codable, Hashable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GHFollowers/Screens/FavoritesListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListViewController.swift
+//  GHFollowers
+//
+//  Created by Alexander on 09.08.2021.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+    }
+}
