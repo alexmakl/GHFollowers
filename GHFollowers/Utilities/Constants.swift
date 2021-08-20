0d1bf17f715e649e7370ba62610feeb0d10a2607
Alexander Maklakov <richmak@ya.ru>
2021-08-20 22:09:07 +0300

    GFUserInfoHeaderVC added to UserInfoVC

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Alexander on 20.08.2021.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
